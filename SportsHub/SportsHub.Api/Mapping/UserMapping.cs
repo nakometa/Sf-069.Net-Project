e4919c0c5d7120a930215a53598e39eee2f94efb
Svetoslav Zhekov <svetlyo.zhekov@gmail.com>
2022-09-30 17:37:07 +0300

    Renamed Profiles to Mapping and UserProfile to UserMapping.

---
+++ SportsHub/SportsHub.Api/Mapping/UserMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SportsHub.Api.DTOs;
+using SportsHub.Domain.Models;
+
+namespace SportsHub.Api.Profiles;
+
+/// <summary>
+/// Class <c>UserMapping</c> acts as a mapping profile for the <c>User</c> entity.
+/// </summary>
+
+public class UserMapping : Profile
+{
+    public UserMapping()
+    {
+        CreateMap<User, UserResponseDTO>();
+    }
+}

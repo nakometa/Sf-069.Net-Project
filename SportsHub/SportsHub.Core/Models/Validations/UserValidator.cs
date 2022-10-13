70088db81947848c01f7d81041b50a467063e004
martuto31 <martuto31@gmail.com>
2022-10-13 12:13:58 +0300

    Rules updated in UserValidator.

--- SportsHub/SportsHub.Core/Models/Validations/UserValidator.cs
+++ SportsHub/SportsHub.Core/Models/Validations/UserValidator.cs
@@ -11,12 +11,14 @@ namespace SportsHub.Domain.Models.Validations
     {
         public UserValidator()
         {
-            RuleFor(t => t.Email).EmailAddress();
-            RuleFor(t => t.Username).NotEmpty();
-            RuleFor(t => t.DisplayName).NotEmpty();
-            RuleFor(t => t.FirstName).NotEmpty();
-            RuleFor(t => t.LastName).NotEmpty();
-            RuleFor(t => t.Password).NotEmpty();
+            RuleFor(t => t.Email).EmailAddress().Length(0, 100);
+            // Username must contain at least 6 characters
+            RuleFor(t => t.Username).NotEmpty().Length(6, 100);
+            RuleFor(t => t.DisplayName).NotEmpty().Length(0, 100);
+            RuleFor(t => t.FirstName).NotEmpty().Length(0, 100);
+            RuleFor(t => t.LastName).NotEmpty().Length(0, 100);
+            // Password must contain at least 8 characters
+            RuleFor(t => t.Password).NotEmpty().Length(8, 100);
         }
     }
 }

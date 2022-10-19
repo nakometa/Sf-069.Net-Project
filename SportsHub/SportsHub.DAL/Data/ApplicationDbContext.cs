3ebe46bf543d82dda4de30ec2c4479faf61bca94
Banovvv <ivan_banov@abv.bg>
2022-10-19 12:30:50 +0300

    ApplicationDbContext updates

--- SportsHub/SportsHub.DAL/Data/ApplicationDbContext.cs
+++ SportsHub/SportsHub.DAL/Data/ApplicationDbContext.cs
@@ -16,6 +16,10 @@ namespace SportsHub.DAL.Data
 
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<State> States { get; set; }
+        public virtual DbSet<Article> Articles { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -25,6 +29,10 @@ namespace SportsHub.DAL.Data
         {
             new UserEntityTypeConfiguration().Configure(modelBuilder.Entity<User>());
             new RoleEntityTypeConfiguration().Configure(modelBuilder.Entity<Role>());
+            new StateEntityTypeConfiguration().Configure(modelBuilder.Entity<State>());
+            new ArticleEntityTypeConfiguration().Configure(modelBuilder.Entity<Article>());
+            new CommentEntityTypeConfiguration().Configure(modelBuilder.Entity<Comment>());
+            new CategoryEntityTypeConfiguration().Configure(modelBuilder.Entity<Category>());
         }
     }
 }

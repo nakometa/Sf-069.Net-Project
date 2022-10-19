8de3c5ca6a95fe2ec27d5d33c4e0d953bac3eb71
Deivit Cholakov <azazel7298@gmail.com>
2022-10-19 18:34:58 +0300

    created a unit test for UserService

---
+++ SportsHub/UnitTests/Services/TestUserService.cs
@@ -0,0 +1,107 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using SportsHub.AppService.Services;
+using SportsHub.DAL.Data;
+using SportsHub.DAL.UOW;
+using SportsHub.Domain.Models;
+using SportsHub.Domain.UOW;
+using Xunit;
+
+namespace UnitTests.Services;
+
+public class TestUserService: IDisposable
+{
+    private readonly ApplicationDbContext _context;
+    private IUnitOfWork _unitOfWork;
+
+    public TestUserService()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+         _context = new ApplicationDbContext(options);
+        _context.Database.EnsureCreated();
+        
+    }
+
+    [Theory]
+    [InlineData("firstuser@mail.com")]
+    [InlineData("seconduser@mail.com")]
+    public async Task GetByEmailAsync_Should_Work_Properly(string email)
+    {
+        //Arrange
+        _context.Users.AddRange(MockData.UsersMockData.GetUsers());
+        _unitOfWork = new UnitOfWork(_context);
+        _unitOfWork.SaveChangesAsync();        
+
+        var sut = new UserService(_unitOfWork);
+
+        //Act
+        var result = await sut.GetByEmailAsync(email);
+        
+        //Assert
+        Assert.Equal(email, result.Email);
+    }
+
+    [Theory]
+    [InlineData("email@email.com")]
+    [InlineData(null)]
+    public async Task GetByEmailAsync_Should_Return_Null(string email)
+    {
+        //Arrange
+        _context.Users.AddRange(MockData.UsersMockData.GetUsers());
+        _unitOfWork = new UnitOfWork(_context);
+        _unitOfWork.SaveChangesAsync();        
+
+        var sut = new UserService(_unitOfWork);
+
+        //Act
+        var result = await sut.GetByEmailAsync(email);
+        
+        //Assert
+        result.Should().BeNull();
+    }
+    
+    [Theory]
+    [InlineData("gogo")]
+    [InlineData("niki")]
+    public async Task GetByUsernameAsync_Should_Work_Properly(string userName)
+    {
+        //Arrange
+        _context.Users.AddRange(MockData.UsersMockData.GetUsers());
+        _unitOfWork = new UnitOfWork(_context);
+        _unitOfWork.SaveChangesAsync();        
+
+        var sut = new UserService(_unitOfWork);
+
+        //Act
+        var result = await sut.GetByUsernameAsync(userName);
+        
+        //Assert
+        Assert.Equal(userName, result.Username);
+    }
+
+    [Theory]
+    [InlineData("peshkata")]
+    public async Task GetByUsernameAsync_Should_Return_Null(string userName)
+    {
+        //Arrange
+        _context.Users.AddRange(MockData.UsersMockData.GetUsers());
+        _unitOfWork = new UnitOfWork(_context);
+        _unitOfWork.SaveChangesAsync();        
+
+        var sut = new UserService(_unitOfWork);
+
+        //Act
+        var result = await sut.GetByUsernameAsync(userName);
+        
+        //Assert
+        result.Should().BeNull();
+    }
+    
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _unitOfWork.Dispose();
+    }
+}

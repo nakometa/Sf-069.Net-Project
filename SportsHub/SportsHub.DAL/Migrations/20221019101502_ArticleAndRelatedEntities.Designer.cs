fd972fb1643c84822ab87a846ae1dce18c47c1eb
nakometa <kruseryyy@gmail.com>
2022-10-19 13:20:18 +0300

    Added Migration

--- SportsHub/SportsHub.DAL/Migrations/20221019101502_ArticleAndRelatedEntities.Designer.cs
+++ SportsHub/SportsHub.DAL/Migrations/20221019101502_ArticleAndRelatedEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsHub.DAL.Data;
 
@@ -11,9 +12,10 @@ using SportsHub.DAL.Data;
 namespace SportsHub.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221019101502_ArticleAndRelatedEntities")]
+    partial class ArticleAndRelatedEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

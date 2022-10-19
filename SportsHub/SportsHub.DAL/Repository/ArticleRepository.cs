65dec106b3966f519fbf0898fba1bb180a9c6cce
Banovvv <ivan_banov@abv.bg>
2022-10-19 14:06:41 +0300

    ArticleRepository

---
+++ SportsHub/SportsHub.DAL/Repository/ArticleRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsHub.DAL.Data;
+using SportsHub.Domain.Models;
+using SportsHub.Domain.Repository;
+
+namespace SportsHub.DAL.Repository
+{
+    public class ArticleRepository : GenericRepository<Article>, IArticleRepository
+    {
+        public ArticleRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Article?> GetByTitleAsync(string title)
+        {
+            return await DbSet
+                .Where(x => x.Title == title)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Article>> GetByAuthorAsync(string author)
+        {
+            return await DbSet
+                .Where(x => x.Authors.Any(a => a.DisplayName == author))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Article>> GetByCategoryAsync(string category)
+        {
+            return await DbSet
+                .Include(x => x.Category)
+                .Where(x => x.Category.Name == category)
+                .ToListAsync();
+        }
+    }
+}

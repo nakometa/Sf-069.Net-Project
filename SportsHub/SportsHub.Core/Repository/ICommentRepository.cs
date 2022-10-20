11819558893fa0397418f5c883844f12486ea4fe
Banovvv <ivan_banov@abv.bg>
2022-10-20 08:00:34 +0300

    CommentRepository and ICommentRepository

---
+++ SportsHub/SportsHub.Core/Repository/ICommentRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsHub.Domain.Models;
+
+namespace SportsHub.Domain.Repository
+{
+    public interface ICommentRepository
+    {
+        Task<IEnumerable<Comment>> GetByArticleAsync(int id);
+        Task<IEnumerable<Comment>> GetByAuthorAsync(int id);
+        Task<IEnumerable<Comment>> GetByAuthorAsync(string author);
+    }
+}
---
+++ SportsHub/SportsHub.DAL/Repository/CommentRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsHub.DAL.Data;
+using SportsHub.Domain.Models;
+using SportsHub.Domain.Repository;
+
+namespace SportsHub.DAL.Repository
+{
+    public class CommentRepository : GenericRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Comment>> GetByArticleAsync(int id)
+        {
+            return await DbSet
+                .Where(x => x.ArticleId == id)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetByAuthorAsync(int id)
+        {
+            return await DbSet
+                .Where(x => x.AuthorId == id)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Comment>> GetByAuthorAsync(string author)
+        {
+            return await DbSet
+                .Include(x => x.Author)
+                .Where(x => x.Author.DisplayName == author)
+                .ToListAsync();
+        }
+    }
+}

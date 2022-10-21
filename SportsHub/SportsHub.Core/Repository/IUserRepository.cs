c7268074f9cfda9e1fbb82fa7554bff0bf660a4b
Ivan Gechev <ivan_banov@abv.bg>
2022-10-21 17:26:44 +0300

    Getting user from db with one call from either username or email

--- SportsHub/SportsHub.Core/Repository/IUserRepository.cs
+++ SportsHub/SportsHub.Core/Repository/IUserRepository.cs
@@ -6,6 +6,7 @@ namespace SportsHub.Domain.Repository
     {
         public Task<User?> GetByUsernameAsync(string username);
         public Task<User?> GetByEmailAsync(string email);
+        public Task<User?> GetByUsernameOrEmailAsync(string usernameOrEmail);
         public Task SaveUserAsync(User user);
     }
 }

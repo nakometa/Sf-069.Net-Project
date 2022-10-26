8ab7af52f3e6c189001f94b5bf0abca364853ddb
martuto31 <martuto31@gmail.com>
2022-10-26 15:36:52 +0300

    ArticlePicture added to Article model.

--- SportsHub/SportsHub.DAL/Migrations/20221026123612_ArticlePictureColumnAddedToArticleTable.Designer.cs
+++ SportsHub/SportsHub.DAL/Migrations/20221026123612_ArticlePictureColumnAddedToArticleTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsHub.DAL.Data;
 
@@ -11,9 +12,10 @@ using SportsHub.DAL.Data;
 namespace SportsHub.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221026123612_ArticlePictureColumnAddedToArticleTable")]
+    partial class ArticlePictureColumnAddedToArticleTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

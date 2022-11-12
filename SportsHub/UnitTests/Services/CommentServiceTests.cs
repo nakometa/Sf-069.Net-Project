803e1260afbc8d1ed100bbdf10196e4c5af0c449
Ivan Gechev <ivan_banov@abv.bg>
2022-11-12 09:47:45 +0200

    Setting up tests

---
+++ SportsHub/UnitTests/Services/CommentServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using SportsHub.AppService.Services;
+using SportsHub.Domain.Repository;
+using SportsHub.Domain.UOW;
+
+namespace UnitTests.Services
+{
+    public class CommentServiceTests
+    {
+        private readonly CommentService _commentService;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+        private readonly Mock<ICommentRepository> _repository;
+
+        public CommentServiceTests()
+        {
+            _unitOfWorkMock = new Mock<IUnitOfWork>();
+            _repository = new Mock<ICommentRepository>();
+            _unitOfWorkMock.Setup(u => u.CommentRepository).Returns(_repository.Object);
+            _commentService = new CommentService(_unitOfWorkMock.Object);
+        }
+    }
+}

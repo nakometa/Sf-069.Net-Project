0d9d65a37120bf85004d7e38cc99b0285da032f8
Ivan Gechev <ivan_banov@abv.bg>
2022-11-13 19:58:04 +0200

    Migration

---
+++ SportsHub/SportsHub.DAL/Migrations/20221113175450_LikeDislikeComments.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsHub.DAL.Migrations
+{
+    public partial class LikeDislikeComments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Dislikes",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Likes",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dislikes",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "Likes",
+                table: "Comments");
+        }
+    }
+}

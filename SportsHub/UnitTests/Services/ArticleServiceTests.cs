4178dc693cca3b4d15ddb96593637db077102332
Deivit Cholakov <azazel7298@gmail.com>
2022-11-19 10:49:29 +0200

    Implementing unit tests from the beginning for ArticleServiceTests because of the new logic in this service

--- SportsHub/UnitTests/Services/ArticleServiceTests.cs
+++ SportsHub/UnitTests/Services/ArticleServiceTests.cs
@@ -1,5 +1,13 @@
-﻿using Moq;
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using AutoMapper;
+using Moq;
+using SportsHub.Api.Exceptions.CustomExceptionModels;
+using SportsHub.Api.Mapping;
 using SportsHub.AppService.Services;
+using SportsHub.Domain.Models;
+using SportsHub.Domain.Models.Constants;
 using SportsHub.Domain.Repository;
 using SportsHub.Domain.UOW;
 using Xunit;
@@ -11,40 +19,72 @@ public class ArticleServiceTests
     private readonly Mock<IUnitOfWork> _unitOfWork;
     private readonly Mock<IArticleRepository> _repository;
     private readonly ArticleService _service;
+    private readonly IMapper _mapper;
+    private IFixture _fixture;
 
     public ArticleServiceTests()
     {
-        _repository = new Mock<IArticleRepository>();
-        _unitOfWork = new Mock<IUnitOfWork>();
+        SetupFixture();
+        _repository = _fixture.Freeze<Mock<IArticleRepository>>();
+        _unitOfWork =_fixture.Freeze<Mock<IUnitOfWork>>();
         _unitOfWork.Setup(u => u.ArticleRepository).Returns(_repository.Object);
-        _service = new ArticleService(_unitOfWork.Object);
+        _mapper = new MapperConfiguration(cfg => cfg.AddProfiles(new List<Profile>()
+        {
+            new ArticleMapping()
+        })).CreateMapper();
+        _service = new ArticleService(_unitOfWork.Object, _mapper);
     }
 
-    [Theory]
-    [InlineData(2)]
-    public async Task DeleteArticleAsync_WithCorrectId_ShouldReturnTrue(int id)
+    [Fact]
+    public async Task GetAllAsync_ReturnAllArticles()
     {
         //Arrange
-        _repository.Setup(r => r.DeleteArticleAsync(id)).ReturnsAsync(true);
-
+        IEnumerable<Article> articles = _fixture.Build<Article>().CreateMany(4);
+        _unitOfWork.Setup(x => x.ArticleRepository.GetAllAsync()).ReturnsAsync(articles);
+        
         //Act
-        var result = await _service.DeleteArticleAsync(id);
-      
+        var result = await _service.GetAllAsync();
+        
         //Assert
-        Assert.Equal(true, result);
+        Assert.Equal(articles.Count(), result.Count());
     }
     
     [Theory]
-    [InlineData(6)]
-    public async Task DeleteArticleAsync_WithoutCorrectId_ShouldReturnFalse(int id)
+    [AutoData]
+    public async Task GetByTitleAsync_WithExistingTitle_ReturnsArticle(string title)
     {
         //Arrange
-        _repository.Setup(r => r.DeleteArticleAsync(id)).ReturnsAsync(false);
-
+        var article = _fixture.Build<Article>().With(x => x.Title, title).Create();
+        _unitOfWork.Setup(x => x.ArticleRepository.GetByTitleAsync(title)).ReturnsAsync(article);
+        
         //Act
-        var result = await _service.DeleteArticleAsync(id);
-      
+        var result = await _service.GetByTitleAsync(title);
+        
         //Assert
-        Assert.Equal(false, result);
+        Assert.Equal(result.Title, article.Title);
     }
+
+    [Theory]
+    [AutoData]
+    public async Task GetByTitleAsync_WithWrongTitle_ReturnsException(string title)
+    {
+        //Arrange
+        _unitOfWork.Setup(x => x.ArticleRepository.GetByTitleAsync(title)).ReturnsAsync((Article?)null);
+        
+        //Act
+        var exception = Assert.ThrowsAsync<NotFoundException>(()=> _service.GetByTitleAsync(title));
+        
+        //Assert
+        Assert.Equal(exception.Result.ErrorCode, StatusCodeConstants.NotFound);
+        Assert.Equal(exception.Result.Message, string.Format(ExceptionMessages.NotFound,ExceptionMessages.Article));
+    }
+    
+    private void SetupFixture()
+    {
+        _fixture = new Fixture().Customize(new AutoMoqCustomization());
+        _fixture.Behaviors.OfType<ThrowingRecursionBehavior>().ToList()
+            .ForEach(b => _fixture.Behaviors.Remove(b));
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
 }

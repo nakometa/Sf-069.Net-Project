5528c11bdb3f1c03b32e4d1dcb4416b70fcfac32
Martin Petrov <martuto31@gmail.com>
2022-11-25 14:33:40 +0200

    Added CommentService tests for retrieving comments methods.

--- SportsHub/UnitTests/Services/CommentServiceTests.cs
+++ SportsHub/UnitTests/Services/CommentServiceTests.cs
@@ -1,9 +1,13 @@
-﻿using Moq;
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using Moq;
 using SportsHub.AppService.Services;
+using SportsHub.DAL.Repository;
 using SportsHub.Domain.Models;
 using SportsHub.Domain.Repository;
 using SportsHub.Domain.UOW;
 using UnitTests.MockData;
+using UnitTests.Utils;
 using Xunit;
 
 namespace UnitTests.Services
@@ -14,6 +18,7 @@ namespace UnitTests.Services
         private readonly int TestArticleId = 5;
         private readonly int TestCommentId = 1;
         private readonly CommentService _commentService;
+        private readonly Fixture _fixture;
         private readonly Mock<IUnitOfWork> _unitOfWorkMock;
         private readonly Mock<IUserRepository> _userRepository;
         private readonly Mock<IArticleRepository> _articleRepository;
@@ -31,6 +36,8 @@ namespace UnitTests.Services
             _unitOfWorkMock.Setup(u => u.CommentRepository).Returns(_commentRepository.Object);
 
             _commentService = new CommentService(_unitOfWorkMock.Object);
+            _fixture = new Fixture();
+            _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
         }
 
         [Fact]
@@ -139,6 +146,110 @@ namespace UnitTests.Services
 
             //Assert
             Assert.False(result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void GetByArticle_WithExistingComments_ReturnsCollectionOfComments([Frozen] Mock<IUnitOfWork> uow, CommentService commentService, int commentsCount)
+        {
+            //Arrange
+            var comments = _fixture.Build<Comment>().CreateMany(commentsCount).AsQueryable();
+            uow.Setup(repo => repo.CommentRepository.GetByArticle(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Returns(comments);
+
+            //Act
+            var result = commentService.GetByArticle(It.IsAny<int>(), It.IsAny<CategoryParameters>());
+
+            //Assert
+            Assert.Equal(result.Count(), commentsCount);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void GetByArticle_WithNonExistingComments_ThrowsException([Frozen] Mock<IUnitOfWork> uow, CommentService commentService)
+        {
+            //Arrange
+            uow.Setup(repo => repo.CommentRepository.GetByArticle(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Throws<Exception>();
+
+            //Assert
+            Assert.Throws<Exception>(() => commentService.GetByArticle(It.IsAny<int>(), It.IsAny<CategoryParameters>()));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void GetByArticleOrderByDate_WithExistingComments_ReturnsSortedCollection([Frozen] Mock<IUnitOfWork> uow, CommentService commentService, int commentsCount)
+        {
+            //Arrange
+            var comments = _fixture.Build<Comment>().CreateMany(commentsCount).AsQueryable();
+            uow.Setup(repo => repo.CommentRepository.OrderByDate(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Returns(comments);
+
+            //Act
+            var result = commentService.GetByArticleOrderByDate(It.IsAny<int>(), It.IsAny<CategoryParameters>()).ToList();
+
+            //Assert
+            Assert.Equal(result.Count(), commentsCount);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void GetByArticleOrderByDate_WithNonExistingComments_ThrowsException([Frozen] Mock<IUnitOfWork> uow, CommentService commentService)
+        {
+            //Arrange
+            uow.Setup(repo => repo.CommentRepository.OrderByDate(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Throws<Exception>();
+
+            //Assert
+            Assert.Throws<Exception>(() => commentService.GetByArticleOrderByDate(It.IsAny<int>(), It.IsAny<CategoryParameters>()));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void GetByArticleOrderByDateDescending_WithExistingComments_ReturnsCollection([Frozen] Mock<IUnitOfWork> uow, CommentService commentService, int commentsCount)
+        {
+            //Arrange
+            var comments = _fixture.Build<Comment>().CreateMany(commentsCount).AsQueryable();
+            uow.Setup(repo => repo.CommentRepository.OrderByDateDescending(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Returns(comments);
+
+            //Act
+            var result = commentService.GetByArticleOrderByDateDescending(It.IsAny<int>(), It.IsAny<CategoryParameters>()).ToList();
+
+            //Assert
+            Assert.Equal(result.Count(), commentsCount);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void GetByArticleOrderByDateDescending_WithNonExistingComments_ThrowsException([Frozen] Mock<IUnitOfWork> uow, CommentService commentService)
+        {
+            //Arrange
+            uow.Setup(repo => repo.CommentRepository.OrderByDateDescending(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Throws<Exception>();
+
+            //Assert
+            Assert.Throws<Exception>(() => commentService.GetByArticleOrderByDateDescending(It.IsAny<int>(), It.IsAny<CategoryParameters>()));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void SortByLikes_WithExistingComments_ReturnsCollection([Frozen] Mock<IUnitOfWork> uow, CommentService commentService, int commentsCount)
+        {
+            //Arrange
+            var comments = _fixture.Build<Comment>().CreateMany(commentsCount).AsQueryable();
+            uow.Setup(repo => repo.CommentRepository.SortByLikes(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Returns(comments);
+
+            //Act
+            var result = commentService.SortByLikes(It.IsAny<int>(), It.IsAny<CategoryParameters>()).ToList();
+
+            //Assert
+            Assert.Equal(result.Count(), commentsCount);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void SortByLikes_WithNonExistingComments_ThrowsException([Frozen] Mock<IUnitOfWork> uow, CommentService commentService)
+        {
+            //Arrange
+            uow.Setup(repo => repo.CommentRepository.SortByLikes(It.IsAny<int>(), It.IsAny<CategoryParameters>())).Throws<Exception>();
+
+            //Assert
+            Assert.Throws<Exception>(() => commentService.SortByLikes(It.IsAny<int>(), It.IsAny<CategoryParameters>()));
         }
     }
 }

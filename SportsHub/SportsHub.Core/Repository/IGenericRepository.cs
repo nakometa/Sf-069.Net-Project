e890a6f55c5185d877adcc5071860b4a08af25a6
Deyvit Cholakov <azazel7298@gmail.com>
2023-01-05 14:15:37 +0200

    Made the needed change in IGenericRepository

--- SportsHub/SportsHub.Core/Repository/IGenericRepository.cs
+++ SportsHub/SportsHub.Core/Repository/IGenericRepository.cs
@@ -3,9 +3,8 @@
     public interface IGenericRepository<TEntity>
         where TEntity : class
     {
-        TEntity? GetById(int id);
-        IEnumerable<TEntity> GetAll();
-        void Add(TEntity entity);
+        Task<TEntity?> FindByIdAsync(int id);
+        Task<IEnumerable<TEntity>> FindAllAsync();
         Task AddAsync(TEntity entity);
         void Delete(TEntity entity);
     }

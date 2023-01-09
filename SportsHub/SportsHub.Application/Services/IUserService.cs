d2eb915122d098e7b1351f5c392ab25666bf03f1
Atanas Kirilov <kruseryyy@gmail.com>
2023-01-09 18:34:06 +0200

    Added check methods for user and email because register was broken.

--- SportsHub/SportsHub.Application/Services/IUserService.cs
+++ SportsHub/SportsHub.Application/Services/IUserService.cs
@@ -6,7 +6,9 @@ namespace SportsHub.AppService.Services
     public interface IUserService
     {
         public Task<User?> GetByUsernameAsync(string username);
+        public Task<bool> CheckUsernameAsync(string username);
         public Task<User?> GetByEmailAsync(string email);
+        public Task<bool> CheckEmailAsync(string email);
         public Task<User?> GetByEmailOrUsernameAsync(string usernameOrEmail);
         public Task<User?> GetUserByClaimsAsync(ClaimsIdentity identity);
         public Task SaveUserAsync(User user);
